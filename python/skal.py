cf83851a4035d4a9f21d6a4e5ac714dd7ae1cc96
Kazuhiro Harigai <kharigai@gmail.com>
2020-08-22 18:28:04 +0900

    Add array operation to skal.py

--- python/skal.py
+++ python/skal.py
@@ -44,3 +44,28 @@ lst = [[int(n) for n in input().split()] for i in range(k)]
 [[10, 11, 12], [20, 21, 22], [30, 31, 32]]
 
 
+## 配列ソート
+>>> lst = [7, 9, 10, 8]
+>>> lst.sort()
+>>> lst
+[7, 8, 9, 10]
+>>> lst.sort(reverse=True)
+>>> lst
+[10, 9, 8, 7]
+
+## ソートした配列を作成する
+>>> lst = sorted([7, 9, 10, 8])
+>>> lst
+[7, 8, 9, 10]
+
+## 配列の sum
+>>> sum(lst)
+34
+
+## 配列の最大値
+>>> max(lst)
+10
+
+## 配列の最小値
+>>> max(lst)
+7
