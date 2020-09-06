e8950aad2c67c5d793c6c53f81c54589d772c361
Kazuhiro Harigai <kharigai@gmail.com>
2020-09-06 15:40:58 +0900

    Refactoring abc177_c.py

--- python/abc177_c.py
+++ python/abc177_c.py
@@ -2,13 +2,13 @@ n = int(input())
 lst = list(map(int, input().split()))
 mod = 10**9+7
 
-# 累積和リストの作成
-sums = [0]*(n+1)
+# 累積和(Cumulative sum)リストの作成
+cumsum = [0]*(n+1)
 for i in range(n):
-    sums[i+1] = lst[i] + sums[i]
+    cumsum[i+1] = lst[i] + cumsum[i]
 
 # 題意より、lst の1つ前までが対象
 ans = 0
 for i in range(n-1):
-    ans += (sums[n] - sums[i+1]) * lst[i]
+    ans += (cumsum[n] - cumsum[i+1]) * lst[i]
 print(ans % mod)
